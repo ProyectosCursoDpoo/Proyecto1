d8fd149f919c1fafc4c3e4817127bedc50553711
algalindoc <al.galindo@uniandes.edu.co>
2023-03-28 20:03:52 -0500

    Clase plato

--- entrega3/proyecto1_hotel/src/logica/Plato.java
+++ entrega3/proyecto1_hotel/src/logica/Plato.java
@@ -1,4 +1,51 @@
 package logica;
 public class Plato {
     
+    public String nombrePlato;
+    public String nombreBebida;
+    public int precio;
+    public String rangoHora;
+    public String lugar;
+
+
+    public String getNombrePlato() {
+        return nombrePlato;
+    }
+
+    public void setNombrePlato(String nombrePlato) {
+        this.nombrePlato = nombrePlato;
+    }
+
+    public String getNombreBebida() {
+        return nombreBebida;
+    }
+
+    public void setNombreBebida(String nombreBebida) {
+        this.nombreBebida = nombreBebida;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public String getRangoHora() {
+        return rangoHora;
+    }
+
+    public void setRangoHora(String rangoHora) {
+        this.rangoHora = rangoHora;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+
 }
