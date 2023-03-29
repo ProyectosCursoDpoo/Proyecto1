8043bafb2b98a3fb4e924531f6bf5e6967d7ae63
RuizL25 <99106138+RuizL25@users.noreply.github.com>
2023-03-28 23:08:09 -0500

    get y set para que deje de salir el
    warning ese canson

--- entrega3/proyecto1_hotel/src/logica/Empleado.java
+++ entrega3/proyecto1_hotel/src/logica/Empleado.java
@@ -1,6 +1,21 @@
 package logica;
 
 public abstract class Empleado {
-    public String usuario ;
-    private String contrasena ;
+    public String usuario;
+    private String contrasena;
+
+    /**
+     * @return String return the contrasena
+     */
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    /**
+     * @param contrasena the contrasena to set
+     */
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
 }
