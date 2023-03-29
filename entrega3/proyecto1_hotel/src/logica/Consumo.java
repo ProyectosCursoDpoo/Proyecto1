ed2a86811930abab36d21b689dd5621c2fbece8d
algalindoc <al.galindo@uniandes.edu.co>
2023-03-28 22:07:10 -0500

    cambios en consumo pqsi

--- entrega3/proyecto1_hotel/src/logica/Consumo.java
+++ entrega3/proyecto1_hotel/src/logica/Consumo.java
@@ -1,112 +1,48 @@
 package logica;
+import java.util.ArrayList;
+
 
 public class Consumo {
 
-    public String fecha;
-    public Grupo grupo;
+    public ArrayList<Huesped> huesped;
     public Servicios servicios;
-    public Habitacion habitacion;
-    public int impuesto;
-    public int tarifaTotal;
-    public boolean pagado;
-    public Factura factura;
+    public int precioIndv;
 
 
-    public Consumo(String fecha, Grupo grupo, Servicios servicios, Habitacion habitacion, int impuesto, int tarifaTotal,
-            boolean pagado, Factura factura) {
-        this.fecha = fecha;
-        this.grupo = grupo;
+    public Consumo(ArrayList<Huesped> huesped, Servicios servicios) {
+        this.huesped = huesped;
         this.servicios = servicios;
-        this.habitacion = habitacion;
-        this.impuesto = impuesto;
-        this.tarifaTotal = tarifaTotal;
-        this.pagado = pagado;
-        this.factura = factura;
-
-
+        this.precioIndv = servicios.getPrecio();
         
     }
 
-
-    public void setFecha(String fecha) {
-        this.fecha = fecha;
+    public void sethuesped(ArrayList<Huesped> huesped) {
+        this.huesped = huesped;
     }
-
-
-    public void setGrupo(Grupo grupo) {
-        this.grupo = grupo;
-    }
-
 
     public void setServicios(Servicios servicios) {
         this.servicios = servicios;
     }
 
-
-    public void setHabitacion(Habitacion habitacion) {
-        this.habitacion = habitacion;
+    public void setprecioIndv(int precioIndv) {
+        this.precioIndv = precioIndv;
     }
 
-
-    public void setImpuesto(int impuesto) {
-        this.impuesto = impuesto;
+    public ArrayList<Huesped> gethuesped() {
+        return huesped;
     }
-
-
-    public void setTarifaTotal(int tarifaTotal) {
-        this.tarifaTotal = tarifaTotal;
-    }
-
-
-    public void setPagado(boolean pagado) {
-        this.pagado = pagado;
-    }
-
-
-    public void setFactura(Factura factura) {
-        this.factura = factura;
-    }
-
-
-    public String getFecha() {
-        return fecha;
-    }
-
-
-    public Grupo getGrupo() {
-        return grupo;
-    }
-
 
     public Servicios getServicios() {
         return servicios;
     }
 
-
-    public Habitacion getHabitacion() {
-        return habitacion;
-    }
-
-
-    public int getImpuesto() {
-        return impuesto;
-    }
-
-
-    public int getTarifaTotal() {
-        return tarifaTotal;
-    }
-
-
-    public boolean isPagado() {
-        return pagado;
-    }
-
-
-    public Factura getFactura() {
-        return factura;
+    public int getPrecioIndv() {
+        return precioIndv;
     }
 
     
+    //public double getTotal(){
+      //  return precioIndv * huesped.size();
+    //}
     
 }
