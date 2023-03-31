ea1e3f41c25139a121dbb7a9f27c35de684d619a
RuizL25 <99106138+RuizL25@users.noreply.github.com>
2023-03-30 22:07:26 -0500

    primer avance metodo iniciar reserva

--- entrega3/proyecto1_hotel/src/logica/Recepcionista.java
+++ entrega3/proyecto1_hotel/src/logica/Recepcionista.java
@@ -1,6 +1,8 @@
 package logica;
 
-import java.util.Date;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 //import java.util.ArrayList;
 
 public class Recepcionista extends Empleado {
@@ -14,11 +16,45 @@ public class Recepcionista extends Empleado {
         this.nombre = nombre;
     }
 
-    public reserva crear_reserva(int numero_reserva, Grupo grupo, int tarifa_reserva, String estado,
-            Date fecha_realizada, String rango_fecha_reserva, Empleado empleado) {
-        reserva reserva = new reserva(numero_reserva, grupo, tarifa_reserva, estado, fecha_realizada,
-                rango_fecha_reserva, empleado);
-        return reserva;
+    public void iniciarReserva(HashMap<Integer, Huesped> huespedes, HashMap<Integer, reserva> reservas,
+            HashMap<Integer, Habitacion> habitaciones) {
+        // creo otro arraylist
+        ArrayList<Huesped> huespedes_reserva = new ArrayList<Huesped>();
+
+        // pregunto por los huespedes
+
+        System.out.println("Vamos a empezar con agregar la informacion de los huespedes \n Empecemos: ");
+        do {
+            System.out.println("Ingrese la informacion del huesped: ");
+            int id = Integer.parseInt(input("Ingrese el id del huesped"));
+            System.out.println(huespedes);
+            if (huespedes.containsKey(id)) {
+                System.out.println("El huesped ya existe");
+                huespedes_reserva.add(huespedes.get(id));
+            } else {
+                System.out.println("El huesped no existe");
+                String nombre_huesped = input("Ingrese el nombre del huesped");
+                String correo = input("Ingrese el correo del huesped");
+                String celular = input("Ingrese el celular del huesped");
+                String fecha = input("Ingrese la fecha de nacimiento del huesped");
+                Huesped nuevo_huesped = new Huesped(nombre_huesped, id, correo, celular, fecha);
+                huespedes.put(id, nuevo_huesped);
+                huespedes_reserva.add(nuevo_huesped);
+            }
+        } while (input("Desea agregar otro huesped? (S/N)").equals("S"));
+        // pregunto la habitacion que quiere
+        System.out.println("Ahora te presentaremos la informacion de las habitaciones para que escojas: ");
+        for (Object k : habitaciones.keySet()) {
+            System.out.println("Habitacion #" + k + ": \n ");
+            Habitacion habitacion = habitaciones.get(k);
+            System.out.println("Ubicacion: " + habitacion.getUbicacion() + "\n");
+            System.out.println("Capacidad: " + habitacion.getCapacidad() + "\n");
+            System.out.println("Camas: " + habitacion.getCamas() + "\n");
+            System.out.println("\n");
+        }
+
+        // numero reserva es el # de habitacion
+
     }
 
     /**
@@ -61,6 +97,16 @@ public class Recepcionista extends Empleado {
      */
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public String input(String mensaje) {
+        try {
+            System.out.print(mensaje + ": ");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            return reader.readLine();
+        } catch (IOException e) {
+            return null;
+        }
     }
 
 }
