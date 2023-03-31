5701fb2b15e53c62e89a86a0c3dc91de47b0c5bd
RuizL25 <99106138+RuizL25@users.noreply.github.com>
2023-03-30 20:23:49 -0500

    Ajuste de la clase recepcionista

--- entrega3/proyecto1_hotel/src/logica/Recepcionista.java
+++ entrega3/proyecto1_hotel/src/logica/Recepcionista.java
@@ -1,21 +1,24 @@
 package logica;
 
-import java.util.ArrayList;
+import java.util.Date;
+//import java.util.ArrayList;
 
 public class Recepcionista extends Empleado {
     private String usuario;
     private String contrasena;
     private String nombre;
 
-    // public Recepcionista(String usuario, String contrasena, String nombre) {
-    //     this.usuario = usuario;
-    //     this.contrasena = contrasena;
-    //     this.nombre = nombre;
-    // }
+    public Recepcionista(String usuario, String contrasena, String nombre) {
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+        this.nombre = nombre;
+    }
 
-    private void iniciarReserva(ArrayList<Huesped> huespedes, ArrayList<Habitacion> habitaciones, String fecha_inicio,
-            String fecha_final) {
-        // implementar codigo
+    public reserva crear_reserva(int numero_reserva, Grupo grupo, int tarifa_reserva, String estado,
+            Date fecha_realizada, String rango_fecha_reserva, Empleado empleado) {
+        reserva reserva = new reserva(numero_reserva, grupo, tarifa_reserva, estado, fecha_realizada,
+                rango_fecha_reserva, empleado);
+        return reserva;
     }
 
     /**
