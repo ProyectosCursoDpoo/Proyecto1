253ecc77b4381dc76032c2aae542fcdb632d9438
RuizL25 <99106138+RuizL25@users.noreply.github.com>
2023-03-31 13:28:49 -0500

    pruebas

--- entrega3/proyecto1_hotel/src/logica/reserva.java
+++ entrega3/proyecto1_hotel/src/logica/reserva.java
@@ -3,142 +3,147 @@ package logica;
 import java.util.ArrayList;
 
 public class reserva {
-    private int numeroReserva;
-    private Grupo grupo;
-    private int tarifaReserva;
-    private String estado;
-    private String fechaRealizada;
-    private String rangoFechaReserva;
-    private Empleado empleado;
-    private ArrayList<Consumo> consumosPendientes;
-    private ArrayList<Consumo> consumosPagos;
-    private double saldoPendiente;
 
-    public reserva(int numeroReserva, Grupo grupo, int tarifaReserva, String fechaRealizada,
-            String rangoFechaReserva,
-            Empleado empleado) {
-        this.numeroReserva = numeroReserva;
-        this.grupo = grupo;
-        this.tarifaReserva = tarifaReserva;
-        this.estado = "INICIADA";
-        this.fechaRealizada = fechaRealizada;
-        this.rangoFechaReserva = rangoFechaReserva;
-        this.empleado = empleado;
-        this.consumosPendientes = new ArrayList<Consumo>();
-        this.consumosPagos = new ArrayList<Consumo>();
-    }
+  private int numeroReserva;
+  private Grupo grupo;
+  private int tarifaReserva;
+  private String estado;
+  private String fechaRealizada;
+  private String rangoFechaReserva;
+  private Empleado empleado;
+  private ArrayList<Consumo> consumosPendientes;
+  private ArrayList<Consumo> consumosPagos;
+  private double saldoPendiente;
 
-    public int getNumeroReserva() {
-        return this.numeroReserva;
-    }
+  public reserva(
+    int numeroReserva,
+    Grupo grupo,
+    int tarifaReserva,
+    String fechaRealizada,
+    String rangoFechaReserva,
+    Empleado empleado
+  ) {
+    this.numeroReserva = numeroReserva;
+    this.grupo = grupo;
+    this.tarifaReserva = tarifaReserva;
+    this.estado = "INICIADA";
+    this.fechaRealizada = fechaRealizada;
+    this.rangoFechaReserva = rangoFechaReserva;
+    this.empleado = empleado;
+    this.consumosPendientes = new ArrayList<Consumo>();
+    this.consumosPagos = new ArrayList<Consumo>();
+  }
 
-    public void setNumeroReserva(int numeroReserva) {
-        this.numeroReserva = numeroReserva;
-    }
+  public int getNumeroReserva() {
+    return this.numeroReserva;
+  }
 
-    /**
-     * @return Grupo return the grupo
-     */
-    public Grupo getGrupo() {
-        return grupo;
-    }
+  public void setNumeroReserva(int numeroReserva) {
+    this.numeroReserva = numeroReserva;
+  }
 
-    /**
-     * @param grupo the grupo to set
-     */
-    public void setGrupo(Grupo grupo) {
-        this.grupo = grupo;
-    }
+  /**
+   * @return Grupo return the grupo
+   */
+  public Grupo getGrupo() {
+    return grupo;
+  }
 
-    /**
-     * @return int return the tarifaReserva
-     */
-    public int getTarifaReserva() {
-        return tarifaReserva;
-    }
+  /**
+   * @param grupo the grupo to set
+   */
+  public void setGrupo(Grupo grupo) {
+    this.grupo = grupo;
+  }
 
-    /**
-     * @param tarifaReserva the tarifaReserva to set
-     */
-    public void setTarifaReserva(int tarifaReserva) {
-        this.tarifaReserva = tarifaReserva;
-    }
+  /**
+   * @return int return the tarifaReserva
+   */
+  public int getTarifaReserva() {
+    return tarifaReserva;
+  }
 
-    /**
-     * @return String return the estado
-     */
-    public String getEstado() {
-        return estado;
-    }
+  /**
+   * @param tarifaReserva the tarifaReserva to set
+   */
+  public void setTarifaReserva(int tarifaReserva) {
+    this.tarifaReserva = tarifaReserva;
+  }
 
-    /**
-     * @param estado the estado to set
-     */
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
+  /**
+   * @return String return the estado
+   */
+  public String getEstado() {
+    return estado;
+  }
 
-    /**
-     * @return Date return the fechaRealizada
-     */
-    public String getFechaRealizada() {
-        return fechaRealizada;
-    }
+  /**
+   * @param estado the estado to set
+   */
+  public void setEstado(String estado) {
+    this.estado = estado;
+  }
 
-    /**
-     * @param fechaRealizada the fechaRealizada to set
-     */
-    public void setFechaRealizada(String fechaRealizada) {
-        this.fechaRealizada = fechaRealizada;
-    }
+  /**
+   * @return Date return the fechaRealizada
+   */
+  public String getFechaRealizada() {
+    return fechaRealizada;
+  }
 
-    /**
-     * @return String return the rangoFechaReserva
-     */
-    public String getRangoFechaReserva() {
-        return rangoFechaReserva;
-    }
+  /**
+   * @param fechaRealizada the fechaRealizada to set
+   */
+  public void setFechaRealizada(String fechaRealizada) {
+    this.fechaRealizada = fechaRealizada;
+  }
 
-    /**
-     * @param rangoFechaReserva the rangoFechaReserva to set
-     */
-    public void setRangoFechaReserva(String rangoFechaReserva) {
-        this.rangoFechaReserva = rangoFechaReserva;
-    }
+  /**
+   * @return String return the rangoFechaReserva
+   */
+  public String getRangoFechaReserva() {
+    return rangoFechaReserva;
+  }
 
-    /**
-     * @return Empleado return the empleado
-     */
-    public Empleado getEmpleado() {
-        return empleado;
-    }
+  /**
+   * @param rangoFechaReserva the rangoFechaReserva to set
+   */
+  public void setRangoFechaReserva(String rangoFechaReserva) {
+    this.rangoFechaReserva = rangoFechaReserva;
+  }
 
-    /**
-     * @param empleado the empleado to set
-     */
-    public void setEmpleado(Empleado empleado) {
-        this.empleado = empleado;
-    }
+  /**
+   * @return Empleado return the empleado
+   */
+  public Empleado getEmpleado() {
+    return empleado;
+  }
 
-    public ArrayList<Consumo> getConsumosPendientes() {
-        return consumosPendientes;
-    }
+  /**
+   * @param empleado the empleado to set
+   */
+  public void setEmpleado(Empleado empleado) {
+    this.empleado = empleado;
+  }
 
-    public ArrayList<Consumo> getConsumosPagos() {
-        return consumosPagos;
-    }
+  public ArrayList<Consumo> getConsumosPendientes() {
+    return consumosPendientes;
+  }
 
-    public double getSaldoPendiente() {
-        return saldoPendiente;
-    }
+  public ArrayList<Consumo> getConsumosPagos() {
+    return consumosPagos;
+  }
 
-    public void agregarConsumoPendiente(Consumo consumo) {
-        consumosPendientes.add(consumo);
-        saldoPendiente += consumo.getPrecioTotal(); // iqoprepjdaklsmdakmdqoiwjepqejapwd
-    }
+  public double getSaldoPendiente() {
+    return saldoPendiente;
+  }
 
-    public void agregarConsumoPago(Consumo consumo) {
-        consumosPagos.add(consumo);
-    }
+  public void agregarConsumoPendiente(Consumo consumo) {
+    consumosPendientes.add(consumo);
+    saldoPendiente += consumo.getPrecioTotal(); // iqoprepjdaklsmdakmdqoiwjepqejapwd
+  }
 
+  public void agregarConsumoPago(Consumo consumo) {
+    consumosPagos.add(consumo);
+  }
 }
